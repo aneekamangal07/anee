025089978c0d65a4dbd58f9aa812cc69dad78033
aneekamangal07 <aneekamangal@gmail.com>
2023-05-23 04:05:33 +0530

    gradient-changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { SocialIcon } from "react-social-icons";
 import { SiGmail } from "react-icons/si";
+import eclipse3 from "../images/Eclipse3.svg";
 
 const Contact = () => {
   return (
@@ -10,7 +11,7 @@ const Contact = () => {
         <p className="text-[#00df9a] font-b text-center md:text-4xl sm:text-3xl text-2xl">
           CONTACT
         </p>
-        <div className="pt-[100px]">
+        <div className="pt-[100px] relative">
           <div className="py-16 text-white tracking-widest text-center flex flex-col justify-center items-center m-auto">
             <a
               href="https://mail.google.com/mail/u/0/#inbox?compose=GTvVlcSDbtmMJWHgvrLQQLvsRgGbwBnvsJmLZFHtzNhXHcKqGtFckmWDMkGsCHlrfmbSqHDNwjbzh"
@@ -22,7 +23,7 @@ const Contact = () => {
             <h1 className="p-4">LET'S TALK!</h1>
             <h2 className="font-bold ">aneekamangal@gmail.com</h2>
           </div>
-          <div className="flex align-center justify-center py-4">
+          <div className="flex absolute align-center justify-center py-4">
             {/* socials */}
             <SocialIcon
               className="hover:cursor-pointer hover:scale-150 duration-300"
@@ -48,6 +49,9 @@ const Contact = () => {
             {/* <SocialIcon url= "https://www.leetcode.com/aneekamangal07/" title="Instagram" fgColor="gray" bgColor="transparent"/> */}
           </div>
         </div>
+      </div>
+      <div className="absolute">
+        <img src={eclipse3} alt="" />
       </div>
     </div>
   );
