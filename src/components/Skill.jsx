b7ec1446be6ea43da3fb68048fab3a585f30df42
Aneeka Mangal <aneekamangal@gmail.com>
2023-07-14 18:31:46 +0530

    added images

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -3,11 +3,13 @@ import React from "react";
 const Skill = ({ skillName, skillIcon }) => {
   return (
     <div>
-      <section className={`flex flex-col hover:cursor-pointer hover:scale-150 duration-300`}>
+      <section
+        className={`flex flex-col hover:cursor-pointer hover:scale-150 duration-300`}
+      >
         <img
           src={skillIcon}
           alt={skillName}
-          className="object-contain  md:w-20 md:h-20 w-14 h-14 text-white"
+          className="object-contain md:w-20 md:h-20 w-14 h-14 text-white"
         />
       </section>
     </div>
